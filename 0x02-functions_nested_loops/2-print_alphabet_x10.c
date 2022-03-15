1ed7c22a258b3daf66ec1c758e84212df9265eec
Brendabett <brendabett33@gmail.com>
2022-03-15 05:46:07 -0700

    Alphaben 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,12 @@
 #include "main.h"
-/**
-*print_alphabet_x10 - prints alphabet letters
-* ten times
-*desc: The function uses loop to operate
-*Return: no return
-*/
-void print_alphabet_x10(void)
-{
-int counter = 0;
-int letter;
-while (counter++ <= 9)
-{
-for (letter = a; letter <= z; letter++)
-for (letter = "a"; letter <= "z"; letter++)
-_putchar(letter);
 
-_putchar(10);
-}
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_alphabet_x10();
+    return (0);
 }

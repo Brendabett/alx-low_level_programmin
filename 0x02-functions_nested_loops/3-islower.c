f415f05b0dfd5db6e50f40dee5af23614e1299de
Brendabett <98813715+Brendabett@users.noreply.github.com>
2022-03-15 19:44:04 +0300

    My commit

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,35 +3,4 @@
 
 /**
   * _islower - Checks for lowercase character
-  * @c: The character to be checked
-  *
-  * Return: 1 for lowercase character or 0 for anything else
-  */
-int _islower(int c)
-{
-if (c >= 97 && c <= 122)
-{
-return (1);
-}
-
-return (0);
-}
-=======
-#include "main.h"
-
-/**
-  * _islower - Checks for lowercase character
-  * @c: The character to be checked
-  *
-  * Return: 1 for lowercase character or 0 for anything else
-  */
-int _islower(int c)
-{
-	if (c >= 97 && c <= 122)
-	{
-		return (1);
-	}
-
-	return (0);
-}
->>>>>>> 235b02ce4bf2d91c5375242cee361cb9e3662473
+  

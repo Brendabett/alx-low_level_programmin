1395d4c51e245eb122d4f3f0c08336436a6489b8
Brendabett <98813715+Brendabett@users.noreply.github.com>
2022-03-15 18:45:40 +0300

    My commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,12 +8,12 @@
 void print_times_table(int n)
 {
 int digit, mult, result;
-if (n <= 15 && n >= 0);
+if (n <= 15 && n >= 0)
 {
-for (digit = 0; digit <= n; digit++);
+for (digit = 0; digit <= n; digit++)
 {
 _putchar('0');
-for (mult = 1; mult <= n; mult++);
+for (mult = 1; mult <= n; mult++)
 {
 _putchar(',');
 _putchar(' ');
@@ -27,7 +27,7 @@ if (result >= 100);
 _putchar((result / 100) + '0');
 _putchar((result / 10) % 10 + '0');
 }
-else if (result <= 99 && result >= 10);
+else if (result <= 99 && result >= 10)
 {
 _putchar((result / 10) + '0');
 }

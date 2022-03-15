670ec800e7c116d76c26349e791484f4af9cacdc
Brendabett <brendabett33@gmail.com>
2022-03-15 09:51:02 -0700

    My commit

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "main.h"
 
 /**
@@ -16,22 +15,3 @@ return (1);
 
 return (0);
 }
-=======
-#include "main.h"
-
-/**
-  * _islower - Checks for lowercase character
-  * @c: The character to be checked
-  *
-  * Return: 1 for lowercase character or 0 for anything else
-  */
-int _islower(int c)
-{
-	if (c >= 97 && c <= 122)
-	{
-		return (1);
-	}
-
-	return (0);
-}
->>>>>>> 235b02ce4bf2d91c5375242cee361cb9e3662473

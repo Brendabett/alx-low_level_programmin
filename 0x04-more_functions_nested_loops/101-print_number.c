88ae2965d1adfc4ed3072530e89ba362d3810a4e
Brendabett <98813715+Brendabett@users.noreply.github.com>
2022-03-17 13:00:12 +0300

    My commit

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 /**
  *print_number - print integer.
  *@n: integer.
@@ -17,5 +17,5 @@ x = -n;
 }
 if (x / 10)
 print_number(x / 10);
-_putchar((x % 10) + 0);
+_putchar((x % 10) + '0');
 }

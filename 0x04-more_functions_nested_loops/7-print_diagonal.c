2b04eb40788c7be7433ef95cb0ea351887792d71
Brendabett <98813715+Brendabett@users.noreply.github.com>
2022-03-17 12:02:46 +0300

    My commit

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -17,17 +17,17 @@ for (len = 0; len < n; len++)
 {
 for (space = 0; space < len; space++)
 {
-putchar( );
+putchar(' ');
 }
 
-putchar(\);
+putchar('\\');
 
 if (len == (n - 1))
 {
 continue;
 }
-putchar(n);
+putchar('\n');
 }
 }
-putchar(n);
+putchar('\n');
 }

0cc4f1b359b8e0c93bbf5cd909f78a0fa6184267
Brendabett <98813715+Brendabett@users.noreply.github.com>
2022-03-17 12:08:35 +0300

    My commit

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -17,15 +17,15 @@ for (inc1 = 0; inc1 < size; inc1++)
 {
 for (inc2 = 0; inc2 < (size - 1); inc2++)
 {
-putchar(#);
+putchar('#');
 }
 
-putchar(#);
-putchar(n);
+putchar('#');
+putchar('\n');
 }
 }
 else
 {
-putchar(n);
+putchar('\n');
 }
 }

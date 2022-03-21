4588ac4da9c41b182cd3081f29c2adf0b95f588c
Brendabett <brendabett33@gmail.com>
2022-03-21 10:22:07 -0700

    My commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,17 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
-*reset_to_98 - resets the value of given integer to 98
-*@n: parameter
-*Return : nothing
+* reset_to_98 - reset to 98
+* @n: input value
+*
+* Description: takes a pointer to an int as parameter and updates the value
+* Return: Always (0)
 */
 
 void reset_to_98(int *n)
 {
-main.h *n = 98;
+int *num;
+int x = 98;
+num = &x;
+*n = *num;
 }

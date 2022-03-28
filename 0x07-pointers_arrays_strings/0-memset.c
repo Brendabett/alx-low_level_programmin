0e410adf843c126d0cc0e77794bc7053f83be67e
Brendabett <brendabett33@gmail.com>
2022-03-28 02:21:42 -0700

    My commit

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,21 +1,20 @@
 #include "main.h"
+
 /**
-*_memset - The _memset() function fills
-* the first n bytes of the memory area
-* pointed to by s with the constant byte b
-*@s:target
-*@b: constant byte
-*@n:number of byte
-*Return: returns new value of target
-*/
+ * _memset - fills a memory block with a constant byte
+ * @s: address to memory block
+ * @b: char to be used
+ * @n: number of bytes to be used
+ *
+ * Return: pointer to the memory block
+ */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-main.h while (n)
-main.h {
-main.h main.h s[n - 1] = b;
-main.h main.h n--;
-main.h }
-main.h return (s);
-
+while (n)
+{
+s[n - 1] = b;
+n--;
+}
+return (s);
 }

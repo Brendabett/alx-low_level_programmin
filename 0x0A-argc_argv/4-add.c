de4cd7056ffe3a101dae9574593a2664f823b897
Brendabett <98813715+Brendabett@users.noreply.github.com>
2022-04-01 17:14:39 +0300

    My commit

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
-  *main - adds and prints sum of positive numbers
-  *followed by a new line.
-  *@argc: number of arguments passed.
-  *@argv: pointer to array of size argc, containing
-  *arguments passed.
-  *
-  *Return: 0 on success.
-  *1 on error.
-  */
+*main - adds and prints sum of positive numbers
+*followed by a new line.
+*@argc: number of arguments passed.
+*@argv: pointer to array of size argc, containing
+*arguments passed.
+*
+*Return: 0 on success.
+*1 on error.
+*/
 int main(int argc, char *argv[])
 {
 int count, count2, sum;
@@ -18,7 +18,7 @@ sum = 0;
 
 if (argc < 2)
 {
-printf(0n);
+printf("0\n");
 return (0);
 }
 for (count = 1; count < argc; count++)
@@ -27,12 +27,12 @@ for (count2 = 0; argv[count][count2] != '\0'; count2++)
 {
 if (argv[count][count2] < '0' || argv[count][count2] > '9')
 {
-printf(Errorn);
+printf("Error\n");
 return (1);
 }
 }
 sum += atoi(argv[count]);
 }
-printf(%dn, sum);
+printf("%d\n", sum);
 return (0);
 }

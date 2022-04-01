d52e770246429af3aab6fd37b2093e26eb7bbe97
Brendabett <brendabett33@gmail.com>
2022-04-01 06:41:23 -0700

    My commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - prints the number of arguments
+*@argc: number of arguments
+*@argv:array of arguments
+*Return: the number of arguments
+*/
+
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+0-whatsmyname.c printf("%d\n", argc - 1);
+
+0-whatsmyname.c return (0);
+}

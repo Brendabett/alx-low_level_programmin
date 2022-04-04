da3d79e8cefa6685d195fd0f92fcc76b9fd7ddcc
Brendabett <98813715+Brendabett@users.noreply.github.com>
2022-04-04 11:07:38 +0300

    My commit

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,12 +1,12 @@
-#include "holberton.h"
+#include "main.h"
 #include <stdlib.h>
 /**
-  * free_grid - frees a 2D grid created in alloc_grid.
-  * @grid: pointer to pointer.
-  * @height: height of grid.
-  *
-  * Return: void.
-  */
+* free_grid - frees a 2D grid created in alloc_grid.
+* @grid: pointer to pointer.
+* @height: height of grid.
+*
+* Return: void.
+*/
 void free_grid(int **grid, int height)
 {
 int i;

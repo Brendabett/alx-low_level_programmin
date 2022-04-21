0d1262fd6312ecad23f485cf8f3e811717e0fbf2
Brendabett <brendabett33@gmail.com>
2022-04-21 07:18:22 -0700

    My commit

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,11 +3,13 @@
 void __attribute__((constructor)) hare(void);
 
 /**
-* hare - Prints a string before the
-*   main function is executed.
-*/
+ * hare - Prints a string before the
+ *   main function is executed.
+ */
 void hare(void)
 {
-printf("You're beat! and yet, you must allow,\n"
-"I bore my house upon my back!\n");
-}
+printf(Youre beat! and yet, you must allow,\n"
+"I bore my house upon my back!\n")
+}" > 100-first.c
+)
+

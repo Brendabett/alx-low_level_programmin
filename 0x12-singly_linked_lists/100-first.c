1c702ab1e75e0c41be03dc84e88876b898970024
Brendabett <98813715+Brendabett@users.noreply.github.com>
2022-04-21 17:20:57 +0300

    My commit

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -8,8 +8,6 @@ void __attribute__((constructor)) hare(void);
  */
 void hare(void)
 {
-printf(Youre beat! and yet, you must allow,\n"
-"I bore my house upon my back!\n")
-}" > 100-first.c
-)
-
+printf("You're beat! and yet, you must allow,\n"
+"I bore my house upon my back!\n");
+}

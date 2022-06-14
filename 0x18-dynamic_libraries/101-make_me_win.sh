0de994536a0763a877bf4fca1b7a79b1ca424eb4
Brendabett <98813715+Brendabett@users.noreply.github.com>
2022-06-14 10:51:21 +0300

    My commit

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://raw.githubusercontent.com/Brendabett/alx-low_level_programming/master/0x18-dynamic_libraries/libgigamill.so
-export LD_PRELOAD="$PWD/../libgigamill.so"
+wget -P /tmp/ https://raw.github.com/Brendabett/alx-low_level_programming/master/0x18-dynamic_libraries/libgigamill.so
+export LD_PRELOAD="$PWD/tmp/libgigamill.so"

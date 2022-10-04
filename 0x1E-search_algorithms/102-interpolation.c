17a4f91d3b6ca2fa420c0d57ff16a9c64fc228c6
Brendabett <brendabett33@gmail.com>
2022-10-04 09:52:40 -0700

    My commit

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -1,41 +1,44 @@
 #include "search_algos.h"
 
 /**
- *interpolation_search - function that  searches
- *for a value in a sorted array of integers
- *@array: pointer to the first element of the array to search in
- *@size: number of elements in array
- *@value: the value to search for
- *Return: first index where value is located
+ * interpolation_search - function using Interpolation search algorithm
+ * @array: pointer to array
+ * @size: size of the array
+ * @value: value to search
+ * Return: index where value is found
  */
-
 int interpolation_search(int *array, size_t size, int value)
 {
-size_t i, l, r;
+size_t low;
+size_t high;
+size_t pos;
 
-if (array == NULL)
+if (!array || size == 0)
 return (-1);
 
-for (l = 0, (r = size - 1); r >= l;)
+low = 0;
+high = size - 1;
+while (low < high)
 {
-i = l + (((double)(r - l) / (array[r] - array[l])) * (value - array[l]));
-if (i < size)
+pos = low + (((double)(high - low) /
+(array[high] - array[low])) * (value - array[low]));
+/* check the max limit */
+if (pos > size - 1)
 {
-printf("Value checked array [%ld] = [%d]\n", i, array[i]);
+printf("Value checked array[%lu] is out of range\n", pos);
+return (-1);
 }
+printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
+/* check the min limit */
+if (pos == 0 && array[pos] > value)
+return (-1);
+/* check if the value is found */
+if (array[pos] == value)
+return (pos);
+else if (array[pos] > value)
+high = pos - 1;
 else
-{
-printf("Value checked array [%ld] is out of range\n", i);
-break;
-}
-
-if (array[i] == value)
-return (i);
-if (array[i] > value)
-r = i - 1;
-else
-l = i + 1;
+low = pos + 1;
 }
 return (-1);
-
 }
